2da7301755ea1cb93f1f7ab5e61610a07bac4c42
Aleksandr_Mitin <alex.mitin.psmu@gmail.com>
2022-10-14 16:01:01 +0500

    Update main.ts
    настроил в дашборде разрешения на intents

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,11 @@ export const bot = new Client({
 
   // Discord intents
   intents: [
-    // IntentsBitField.Flags.Guilds,
-    // IntentsBitField.Flags.GuildMembers,
-    // IntentsBitField.Flags.GuildMessages,
-    // IntentsBitField.Flags.GuildMessageReactions,
-    // IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.GuildVoiceStates,
   ],
 
   // Debug logs are disabled in silent mode
