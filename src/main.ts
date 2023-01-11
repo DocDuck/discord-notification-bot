bf1dd388d18dc42c43b1e001b218f478181b8daf
DocDuck <alex.mitin.psmu@gmail.com>
2023-01-11 12:44:47 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { dirname, importx } from '@discordx/importer';
 import type { Interaction, Message } from 'discord.js';
-import { IntentsBitField, WebhookClient } from 'discord.js';
+import {IntentsBitField, TextChannel, WebhookClient} from 'discord.js';
 import { Client } from 'discordx';
 import { schedule } from 'node-cron';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
@@ -43,7 +43,7 @@ export const bot = new Client({
 
 bot.once("ready", async () => {
   // Make sure all guilds are cached
-  // await bot.guilds.fetch();
+  await bot.guilds.fetch();
   // TODO вернуть когда получу бот токен для форсайта,
   //  опционально переделать с вебхука на каналы
   //  await notify()
@@ -53,12 +53,17 @@ bot.once("ready", async () => {
   // To clear all guild commands, uncomment this line,
   // This is useful when moving from guild commands to global commands
   // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
 
-  console.log("Bot started");
+   await bot.clearApplicationCommands(
+     ...bot.guilds.cache.map((g) => g.id)
+   );
+  const voiceRoomID = '1030424287074582581'
+  const voicePingID = '1055401857310269470'
+  const channel = await bot.channels.cache.get(voicePingID)
+
+  channel && schedule('00 11 * * 1-5', async () => {
+    await (channel as TextChannel).send(`@everyone Го на дейлик! <#${voiceRoomID}>`);
+  });
 });
 
 bot.on("interactionCreate", (interaction: Interaction) => {
@@ -76,15 +81,15 @@ async function run() {
 
   // The following syntax should be used in the ECMAScript environment
   // TODO вернуть когда получу бот токен для форсайта
-  // await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+  await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
-  // // Let's start the bot
-  // if (!process.env.BOT_TOKEN) {
-  //   throw Error("Could not find BOT_TOKEN in your environment");
-  // }
-  // // Log in with your bot token
-  // await bot.login(process.env.BOT_TOKEN);
-  await notify()
+  // Let's start the bot
+  if (!process.env.BOT_TOKEN) {
+    throw Error("Could not find BOT_TOKEN in your environment");
+  }
+  // Log in with your bot token
+  await bot.login(process.env.BOT_TOKEN);
+  // await notify()
 }
 
 run();
